2b29db9f1f43c49ba7e470880270d542b5e8959c
jackgala <jackrgala@gmail.com>
2021-04-28 11:36:39 -0400

    refacter(sturcture of code): comments to explain how code will work

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+// convert input of numbers and spaces to a string value of the word of those numbers separated by commas
+
+// array of strings
+let numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+// converted_string string
+let converted_string = undefined;
+
+// function conversion
+
+//  initialize string input
+
+//read in command prompt string into input
+
+//for the length of input
+//get supstring of input by character
+//if it's a space
+//add a comma to converted_string
+//if it's a number(use ascii keys to verify)
+//add the string from a array of strings at index [ascii value - ascii value of '0']
+
+//print out the converted_string
+
+
 
